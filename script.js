cd522ee7f24f9ff86beb8910e7058babdb8bb614
Alexandr <kobal2009@yandex.ru>
2022-12-07 01:49:38 +0500

    Выполненное задание к 4му уроку

--- script.js
+++ script.js
@@ -4,37 +4,53 @@ const title = prompt('Как называется ваш проект?');
 const screens = prompt('Какие типы экранов нужно разработать','Простые, Сложные, Интерактивные');
 const screenPrice = +prompt('Сколько будет стоить данная работа');
 const rollback = 30; //Откат 30%
-const adaptive = !!prompt('Нужен ли адаптив на сайте', 'OK - да, Отмена - нет');
+const adaptive = !!confirm('Нужен ли адаптив на сайте');
 const service1 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice1 = +prompt('Сколько это будет стоить?');
 const service2 = prompt('Какой еще дополнительный тип услуги нужен?');
 const servicePrice2 = +prompt('Сколько это будет стоить?');
-const fullPrice = +(screenPrice + servicePrice1 + servicePrice2);  
-const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
 
-//Выводим данные
+//Сумма всех доп услуг через function expression
+const getAllServicePrices = function() {return servicePrice1 + servicePrice2};
+const allServicePrices = getAllServicePrices();
 
-console.log('Проект: ' + title);
-console.log('Типы экранов: ' + screens);
-console.log('Адаптив: ' + adaptive);
-console.log('Доп услуга 1: ' + service1 + ' Цена: ' + servicePrice1);
-console.log('Доп услуга 2: ' + service2 + ' Цена: ' + servicePrice2);
-console.log('Итоговая стоимость: ' + fullPrice);
-console.log('Итоговая стоимость с учетом отката: ' + servicePercentPrice);
+//Сумма стоимости верстки и стоимости дополнительных услуг через function declaration
+function getFullPrice(screenPrice, allServicePrices) {
+        return screenPrice + allServicePrices;
+}
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
+ 
+//Переводим title в нормальный вид и обрезаем пробелы вначале
+const getTitle = (e) => {
+    e  = title.trimStart(); 
+    return e[0].toUpperCase() + e.slice(1).toLowerCase();
+};
 
+// Итоговая стоимость за вычетом процента отката
+const getServicePercentPrices = () => {return Math.ceil(fullPrice - (fullPrice * (rollback/100)))};
+const servicePercentPrice = getServicePercentPrices(); 
+
+//Типы данных
+const showTypeOf = (v) => {console.log(v, typeof v);};
 
 // Скидки
-switch(true){
-    case fullPrice > 30000 || fullPrice === 30000:
-        console.log('Даем скидку 10%');
-        break
-    case 15000 < fullPrice && fullPrice < 30000 || fullPrice === 15000:
-        console.log('Даем скидку 5%');
-        break
-    case 0 < fullPrice && fullPrice < 15000 || fullPrice === 0:
-        console.log('Скидка не предусмотрена');
-        break
-    case fullPrice < 0:
-        console.log('Что то пошло не так');
-    }
+const getRollbackMessage = function(price) {
+    switch(true){
+        case price > 30000 || price === 30000:
+            return 'Даем скидку 10%';
+        case 15000 < price && price < 30000 || price === 15000:
+            return 'Даем скидку 5%';
+        case 0 < price && price < 15000 || price === 0:
+            return 'Скидка не предусмотрена';
+        case price < 0:
+            return 'Что то пошло не так';
+        }
+};
+
+//Выводим данные
+showTypeOf(getTitle(title));
+showTypeOf(screens);
+console.log('Итоговая стоимость: ' + fullPrice);
+console.log('Итоговая стоимость с учетом отката: ' + servicePercentPrice);
+alert(getRollbackMessage(fullPrice));
 
