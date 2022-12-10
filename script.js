85a136abfcfe9a1c6a2132b0769913d57cb12ac1
Alexandr <kobal2009@yandex.ru>
2022-12-10 19:18:16 +0500

    Домашнее задание к 5му уроку + усложненное №1

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
-'use strict';
-//Спрашиваем клиента и заносим данные
-const title = prompt('Как называется ваш проект?');
-const screens = prompt('Какие типы экранов нужно разработать','Простые, Сложные, Интерактивные');
-const screenPrice = +prompt('Сколько будет стоить данная работа');
-const rollback = 30; //Откат 30%
-const adaptive = !!confirm('Нужен ли адаптив на сайте');
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = +prompt('Сколько это будет стоить?');
-const service2 = prompt('Какой еще дополнительный тип услуги нужен?');
-const servicePrice2 = +prompt('Сколько это будет стоить?');
+'use strict'
+//Объявляем переменные
+let title 
+let screens
+let screenPrice = 0
+let adaptive
+let rollback = 30; //Откат 30%
+let allServicePrices
+let servicePercentPrice
+let fullPrice
+let service1
+let service2 
 
-//Сумма всех доп услуг через function expression
-const getAllServicePrices = function() {return servicePrice1 + servicePrice2};
-const allServicePrices = getAllServicePrices();
-
-//Сумма стоимости верстки и стоимости дополнительных услуг через function declaration
-function getFullPrice(screenPrice, allServicePrices) {
-        return screenPrice + allServicePrices;
+//Проверка на число
+const isNumber = function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num)
 }
-const fullPrice = getFullPrice(screenPrice, allServicePrices);
- 
+
+// Спрашиваем пользователя
+const asking = function() {
+    title = prompt('Как называется ваш проект?', 'Проект')
+    screens = prompt('Какие типы экранов нужно разработать','Простые, Сложные, Интерактивные')
+    while (screenPrice < 1) {
+        screenPrice = prompt('Сколько будет стоить данная работа')
+    }
+    while (!isNumber(screenPrice)){
+        screenPrice = prompt('Сколько будет стоить данная работа')
+    }
+    screenPrice = +screenPrice.trim() // Усложненное задание №1 (получаем число без пробелов)   
+    adaptive = !!confirm('Нужен ли адаптив на сайте')
+}
+
+
+
+//Сумма всех доп услуг
+const getAllServicePrices = function() {
+    let sum = 0
+    for (let i = 0; i < 2; i++) {
+        if (i === 0) {
+            service1 = prompt('Какой дополнительный тип услуги нужен?')
+        } else if (i === 1) {
+            service2 = prompt('Какой еще дополнительный тип услуги нужен?')        
+        }
+        
+        sum += +prompt('Сколько это будет стоить?')
+        while (!isNumber(sum)) {
+            sum = +prompt('Сколько это будет стоить?')
+        }
+    }
+
+    return sum
+}
+
+//Сумма стоимости верстки и стоимости дополнительных услуг
+function getFullPrice() {
+    return screenPrice + allServicePrices
+}
+
 //Переводим title в нормальный вид и обрезаем пробелы вначале
-const getTitle = (e) => {
-    e  = title.trimStart(); 
-    return e[0].toUpperCase() + e.slice(1).toLowerCase();
-};
+const getTitle = function () {
+    return title.trim()[0].toUpperCase() + title.slice(1).toLowerCase()
+}
 
 // Итоговая стоимость за вычетом процента отката
 const getServicePercentPrices = () => {return Math.ceil(fullPrice - (fullPrice * (rollback/100)))};
-const servicePercentPrice = getServicePercentPrices(); 
 
 //Типы данных
 const showTypeOf = (v) => {console.log(v, typeof v);};
@@ -47,10 +81,22 @@ const getRollbackMessage = function(price) {
         }
 };
 
+
+asking()
+title = getTitle()
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+
+
 //Выводим данные
-showTypeOf(getTitle(title));
-showTypeOf(screens);
-console.log('Итоговая стоимость: ' + fullPrice);
-console.log('Итоговая стоимость с учетом отката: ' + servicePercentPrice);
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+showTypeOf(allServicePrices);
+console.log(allServicePrices);
+console.log(servicePercentPrice);
+console.log('Итоговая стоимость: ' + fullPrice + ' руб');
+console.log('Итоговая стоимость с учетом отката: ' + servicePercentPrice + ' руб');
 alert(getRollbackMessage(fullPrice));
 
